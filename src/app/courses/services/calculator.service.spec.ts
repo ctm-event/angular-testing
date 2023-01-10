97d276612d3d414157559cbe2f2d0272cd919ab5
Steven Nguyen <steven.nguyen@contemi.com>
2023-01-10 17:11:47 +0700

    10

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,27 +1,35 @@
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('CalculatorService', () => {
-  let calculator: CalculatorService;
   let loggerSpy: any;
   
   beforeEach(() => {
     console.log('before each');
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {
+          provide: LoggerService,
+          useValue: loggerSpy
+        }
+      ]
+    })
   });
   
   it('should add two numbers', () => {
     console.log('test add two number');
-    const result = calculator.add(2, 3);
+    const result = TestBed.inject(CalculatorService).add(2, 3);
     expect(result).toBe(5);
-    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    expect(TestBed.inject(LoggerService).log).toHaveBeenCalledTimes(1);
   });
 
   it('should substract two numbers', () => {
     console.log('test substract two number');
-    const result = calculator.subtract(5, 2);
+    const result = TestBed.inject(CalculatorService).subtract(5, 2);
     expect(result).toBe(3, 'unexpected substraction');
-    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    expect(TestBed.inject(LoggerService).log).toHaveBeenCalledTimes(1);
   });
 });
