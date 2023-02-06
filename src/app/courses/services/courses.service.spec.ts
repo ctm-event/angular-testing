67c40d963fe9cf3d853896a66bf2bf7d6817cc0b
Steven Nguyen <steven.nguyen@contemi.com>
2023-02-06 16:08:03 +0700

    16

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -4,8 +4,9 @@ import {
   HttpClientTestingModule,
   HttpTestingController
 } from '@angular/common/http/testing';
-import { COURSES } from '../../../../server/db-data';
+import { COURSES, findLessonsForCourse } from '../../../../server/db-data';
 import { Course } from '../model/course';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('CoursesService', () => {
   let coursesService: CoursesService,
@@ -64,6 +65,52 @@ describe('CoursesService', () => {
       ...COURSES[12],
       ...changes
     });
+  });
+
+  it('should give an error if save course fail', () => {
+    const changes: Partial<Course> = {
+      titles: {
+        description: 'Testing Course'
+      }
+    };
+    coursesService.saveCourse(12, changes).subscribe(
+      () => {
+        fail('should return an error');
+      },
+      (error: HttpErrorResponse) => {
+        expect(error.status).toBe(500);
+      }
+    );
+    const req = httpTestingController.expectOne('/api/courses/12');
+
+    expect(req.request.method).toBe('PUT');
+
+    req.flush('Save Course Fail', {
+      status: 500,
+      statusText: 'Internal Server Error'
+    });
+  });
+
+  it('should find a list of lessons', () => {
+    coursesService.findLessons(12).subscribe((lessons) => {
+      expect(lessons).toBeTruthy();
+      expect(lessons.length).toBe(3);
+    });
+
+    const req = httpTestingController.expectOne(
+      (req) => req.url == '/api/lessons'
+    );
+
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.params.get('courseId')).toEqual('12');
+    expect(req.request.params.get('pageSize')).toEqual('3');
+    expect(req.request.params.get('filter')).toEqual('');
+    expect(req.request.params.get('pageNumber')).toEqual('0');
+    expect(req.request.params.get('sortOrder')).toEqual('asc');
+
+    req.flush({
+      payload: findLessonsForCourse(12).slice(0, 3)
+    })
   });
 
   afterEach(() => {
