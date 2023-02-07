5642627f085d5379f195ed8c2aa3b5f15922b711
Steven Nguyen <steven.nguyen@contemi.com>
2023-02-07 15:18:10 +0700

    21

---
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -0,0 +1,46 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { CoursesCardListComponent } from './courses-card-list.component';
+import { CoursesModule } from '../courses.module';
+import { COURSES } from '../../../../server/db-data';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { sortCoursesBySeqNo } from '../home/sort-course-by-seq';
+import { Course } from '../model/course';
+import { setupCourses } from '../common/setup-test-data';
+
+describe('CoursesCardListComponent', () => {
+  let component: CoursesCardListComponent,
+    fixture: ComponentFixture<CoursesCardListComponent>,
+    el: DebugElement;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [CoursesModule]
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+      });
+  }));
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the course list', () => {
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const cards = el.queryAll(By.css('.course-card'));
+
+    console.log(el.nativeElement.outerHTML);
+    
+    expect(cards).toBeTruthy("Cound not find cards");
+    expect(cards.length).toBe(12, 'Unexpected nubmer of courses');
+  });
+
+  xit('should display the first course', () => {
+    pending();
+  });
+});
