8478ecf8631938b535e704879a583f35748c864f
Steven Nguyen <steven.nguyen@contemi.com>
2023-02-21 11:45:51 +0700

    29

--- src/app/courses/home/async-example.spec.ts
+++ src/app/courses/home/async-example.spec.ts
@@ -1,10 +1,21 @@
+import { fakeAsync, flush, tick } from '@angular/core/testing';
+
 fdescribe('Async Testing Example', () => {
   it('Async test example with Jasmine done()', (done: DoneFn) => {
     let test = false;
     setTimeout(() => {
       test = true;
       expect(test).toBeTruthy();
-      done(); 
+      done();
     }, 1000);
   });
+
+  it('Async test example with Angular Zone fakeAsync', fakeAsync(() => {
+    let test = false;
+    setTimeout(() => {
+      test = true;
+    }, 1000);
+    flush();
+    expect(test).toBeTruthy('Unexpected value of test');
+  }));
 });
